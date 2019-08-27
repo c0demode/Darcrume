3f0688bc24db35c731dfc79aa18c46dcd712861b
walderman <william.alderman@gmail.com>
2019-08-27 08:29:58 -0500

    reworked films view to better adapt to different screen sizes

---
+++ app/src/main/java/com/walderman/darcrume/Controller_ManageChems.java
@@ -0,0 +1,9 @@
+package com.walderman.darcrume;
+
+/**
+ * TODO
+ * Show recyclerView. Radio buttons control categories of recyclerView content. BW / Color? Developers / Stops / Fixers/Blix / brands
+ */
+public class Controller_ManageChems {
+
+}
--- app/src/main/java/com/walderman/darcrume/Controller_ManageFilms.java
+++ app/src/main/java/com/walderman/darcrume/Controller_ManageFilms.java
@@ -12,6 +12,12 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 
+/**
+ * TODO
+ * add ability to Add / Save / Delete / Copy
+ * How easy to add circle "+" symbol for Add?
+ */
+
 public class Controller_ManageFilms extends AppCompatActivity {
     //on create, call a method from DatabaseHelper that fills an ArrayList<Film> and thus populates the recyclerview in activity_manage_films.xml
     DatabaseHelper db;
