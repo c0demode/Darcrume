67b198aca02977ead5c52eaf7cf9bcc9018c00c8
walderman <william.alderman@gmail.com>
2019-08-31 11:48:17 -0500

    added and ID'd spinners and such for develop activity. definitely making progress

---
+++ app/src/main/java/com/walderman/darcrume/HoldsMyNotesAndIdeas.java
@@ -0,0 +1,24 @@
+package com.walderman.darcrume;
+
+public class HoldsMyNotesAndIdeas {
+    //store objects as blobs in database
+    //  https://stackoverflow.com/questions/1243181/how-to-store-object-in-sqlite-database
+    /**
+     * Black and White Chems
+     * +Developer
+     * +Stop Bath
+     * +Fixer
+     */
+
+    /**
+     * Color Chems
+     * +Developer
+     * +Blix
+     * +Stabilizer
+     */
+
+    /**
+     * In the long run it may make sense to create tables for different types of chemicals instead of having 1 big table of all chems.
+     * Maybe.
+     */
+}
